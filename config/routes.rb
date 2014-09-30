d585faa8b508cf8a03c14b1defb5905b6605b024
Lion <815694355@qq.com>
2014-09-30 15:01:29 +0800

    路由配置

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Questions::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  get "/aiesecef", :to => 'questions#city_index'
+
   root 'questions#city_index'
 
   # Example of regular route:
